3d70ce1c21406f16b1686de42c2521773c0683eb
Moiz Ali <amoiz4142@gmail.com>
2018-12-18 18:59:08 +0500

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,15 +1,37 @@
 const {SHA256} = require('crypto-js');
 
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+ var password = '123abc!';
 
-var token = jwt.sign(data, '123abc ');
-console.log(token);
-var decoded = jwt.verify(token, '123abc');
-console.log('decoded', decoded);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
+
+var hashedPassword = '$2a$10$YurUZyHm1rel5v4Aj.PRUew.zx.melqByGy3/l3wqSjQ.nD2vQt5i';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// if (hashedPassword.toString() === hashedPassword) {
+//   return console.log(password);
+// }           DONE by Me! But 'else' clause failed to work
+//
+// console.log('Authentication failed!.')
+
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc ');
+// console.log(token);
+// var decoded = jwt.verify(token, '123abc');
+// console.log('decoded', decoded);
 // var message = 'I am user no 3';
 // var hash = SHA256(message).toString();
 //
